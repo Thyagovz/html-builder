5222f4059ccf38bdf80b3e985007e47b5d9b4aea
Thyago Euclides <flooddk@gmail.com>
2026-06-30 10:14:35 -0400

    Refactor Editor and Viewer classes to enhance text editing and visualization functionality

--- Editor.cs
+++ Editor.cs
@@ -1,6 +1,7 @@
 using System.Text;
 
 namespace HtmlBuilder;
+
 public static class Editor
 {
     public static void Show()
@@ -13,16 +14,72 @@ public static class Editor
         Console.WriteLine("-----------");
         Start();
     }
+
     public static void Start()
     {
         var file = new StringBuilder();
 
-        do
+        while (true)
         {
-            file.Append(Console.ReadLine());
-            file.Append(Environment.NewLine);
-        } while (Console.ReadKey().Key != ConsoleKey.Escape);
-        Console.WriteLine("-----------");
-        Console.WriteLine(" Deseja salvar o arquivo?");
+            var keyInfo = Console.ReadKey(true);
+
+            if (keyInfo.Key == ConsoleKey.Escape)
+            {
+                break;
+            }
+
+            if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine();
+                file.Append(Environment.NewLine);
+            }
+            else if (keyInfo.Key == ConsoleKey.Backspace)
+            {
+                if (file.Length > 0)
+                {
+                    file.Remove(file.Length - 1, 1);
+                    Console.Write("\b \b");
+                }
+            }
+            else
+            {
+                Console.Write(keyInfo.KeyChar);
+                file.Append(keyInfo.KeyChar);
+            }
+        }
+
+        Console.WriteLine("\n-----------");
+        Console.WriteLine("Deseja salvar o arquivo? (S/N)");
+
+        var opcao = Console.ReadKey(true).Key;
+
+        if (opcao == ConsoleKey.S)
+        {
+            Salvar(file.ToString());
+        }
+
+        Viewer.Show(file.ToString());
+    }
+
+    public static void Salvar(string text)
+    {
+        Console.Clear();
+        Console.WriteLine("Qual o caminho e nome para salvar o arquivo?");
+        var path = Console.ReadLine();
+
+        try
+        {
+            using (var writer = new StreamWriter(path))
+            {
+                writer.Write(text);
+            }
+            Console.WriteLine("\nArquivo salvo com sucesso!");
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"\nErro ao salvar: {ex.Message}");
+            Console.ReadKey();
+        }
     }
 }
--- Viewer.cs
+++ Viewer.cs
@@ -1,25 +1,39 @@
 using System.Text.RegularExpressions;
 
-namespace HtmlBuilder
+namespace HtmlBuilder;
+
+public class Viewer
 {
-    public class Viewer
+    public static void Show(string text)
     {
-        public static void Show(string text)
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+        Console.WriteLine("MODO VISUALIZAÇÃO");
+        Console.WriteLine("-----------");
+        Replace(text);
+        Console.WriteLine("-----------");
+        Console.ReadKey();
+        Menu.Show();
+    }
+    public static void Replace(string text)
+    {
+        var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>", RegexOptions.IgnoreCase);
+        int lastIndex = 0;
+
+        foreach (Match match in strong.Matches(text))
         {
-            Console.Clear();
-            Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
-            Console.Clear();
-            Console.WriteLine("MODO VISUALIZAÇÃO");
-            Console.WriteLine("-----------");
-            Replace(text);
-            Console.WriteLine("-----------");
-            Console.ReadKey();
-            Menu.Show();
+            Console.Write(text.Substring(lastIndex, match.Index - lastIndex));
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write(match.Groups[1].Value);
+
+            lastIndex = match.Index + match.Length;
         }
-        public static void Replace(string text)
-        {
-              var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
-        }
+
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Write(text.Substring(lastIndex));
     }
 }
