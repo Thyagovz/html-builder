1a7f6e36a6bf12584580683bf74d6a01a697d731
Thyago Euclides <flooddk@gmail.com>
2026-06-30 07:45:54 -0400

    Implement menu option handling in Menu class

--- Menu.cs
+++ Menu.cs
@@ -12,6 +12,7 @@ public static class Menu
         WriteOptions();
 
         var option = short.Parse(Console.ReadLine());
+        HandleMenuOption(option);
     }
 
     public static void DrawScreen()
@@ -59,5 +60,20 @@ public static class Menu
         Console.WriteLine("0 - Sair");
         Console.SetCursorPosition(3, 10);
         Console.WriteLine("Opção: ");
+    }
+    public static void HandleMenuOption(short option)
+    {
+        switch (option)
+        {
+            case 1: Console.WriteLine("Editor"); break;
+            case 2: Console.WriteLine("View"); break;
+            case 0:
+                {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+            default: Show(); break;
+        }
     }
 }
